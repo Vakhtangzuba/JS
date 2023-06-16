34d18f82035ce1fa0fea77b93190fc7c0d0e66fd
Vakhtangzuba <136825281+Vakhtangzuba@users.noreply.github.com>
2023-06-16 18:17:55 +0400

    JavaScript File for master

---
+++ JavaScript/console.js
@@ -0,0 +1,30 @@
+let cvladi = 22
+
+if (cvladi < 50 && cvladi > 20) {
+  console.log("ნაკლებია 50-ზე და მეტია 20-ზე")
+} else if (cvladi  >50 || cvladi < 20) {
+  console.log("error")
+}
+
+
+
+
+let cvladi1 = "მარიამ";
+
+if (cvladi1 = "მარიამ") {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+
+const saxeli = 'mariam';
+switch (saxeli) {
+  case 'mariam':
+    console.log('true');
+    break;
+    default:
+    console.log(`false`);
+
+}
